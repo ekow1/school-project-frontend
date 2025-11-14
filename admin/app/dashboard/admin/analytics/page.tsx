aca46128541f32c3176f5c7c3ad96c07560ac08e
ENOCK YAW ENU <30849211+ekow1@users.noreply.github.com>
2025-11-14 08:00:53 +0000

    fix percent issue at analytics page

--- admin/app/dashboard/admin/analytics/page.tsx
+++ admin/app/dashboard/admin/analytics/page.tsx
@@ -275,7 +275,7 @@ const AdminAnalyticsPage: React.FC = () => {
                   cx="50%"
                   cy="50%"
                   labelLine={false}
-                  label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
+                  label={({ name, percent }: { name: string; percent: number }) => `${name} ${(percent * 100).toFixed(0)}%`}
                   outerRadius={120}
                   innerRadius={70}
                   fill="#8884d8"
